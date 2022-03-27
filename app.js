be44e8b87e66419a170a7770c431d4261141dbfb
martin <martin@comets.kr>
2022-03-28 00:39:57 +0900

    feat: Reja loyihamizning yakuniy bosqich logic

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 // MongoDB choqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -23,6 +24,36 @@ app.post("/create-item", (req, res) => {
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
     res.json(data.ops[0]);
   });
+});
+
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+  db.collection("plans").deleteOne(
+    { _id: new mongodb.ObjectId(id) },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+});
+
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  db.collection("plans").findOneAndUpdate(
+    { _id: new mongodb.ObjectId(data.id) },
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+});
+
+app.post("/delete-all", (req, res) => {
+  if (req.body.delete_all) {
+    db.collection("plans").deleteMany(function () {
+      res.json({ state: "hamma rejalar ochirildi" });
+    });
+  }
 });
 
 app.get("/", function (req, res) {
